0e0d65da9c53ec2a14928c8fce35862206f70519
Debian <admin@ip-172-31-29-20.us-west-1.compute.internal>
2016-03-31 03:29:28 +0000

    GPDB dependencies and quick set-up

---
+++ gpdb/gpdb_setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#sudo apt-get update
+sudo apt-get install -y vim-nox tmux git tree cscope
+
+# Development dependencies
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+
+# Download libraries
+git clone https://github.com/greenplum-db/gpdb
+git clone https://github.com/greenplum-db/gp-orca
+git clone https://github.com/greenplum-db/gpos
+git clone https://github.com/greenplum-db/gp-xerces
+
+
+# GPDB dependencies
+sudo apt-get install -y bison flex
+sudo apt-get install -y libreadline-dev
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y libyaml-dev
+sudo apt-get install -y libapr1-dev
+sudo apt-get install -y libevent-dev
+sudo apt-get install -y libcurl-dev
+sudo apt-get install -y libpam-dev
+sudo apt-get install -y libcurl4-dev
+sudo apt-get install -y libbz2-dev
+sudo apt-get install -y python-dev
+sudo apt-get install -y ssh
+
+sudo apt-get install -y libcurl4-openssl-dev
+
+sudo apt-get install -y python-pip
+
