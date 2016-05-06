fdbed2b9050a41cfe38db31c3d5a9d706776bc5a
Shreedhar Hardikar <shardikar@pivotal.io>
2016-05-06 01:17:20 +0000

    Renamed by os

---
+++ gpdb/gpdb_setup_debian.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#sudo apt-get update
+sudo apt-get install -y vim-nox tmux git tree cscope
+
+# Development dependencies
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+
+# Download libraries
+git clone https://github.com/hardikar/gpdb
+git clone https://github.com/hardikar/gp-orca
+git clone https://github.com/hardikar/gpos
+git clone https://github.com/hardikar/gp-xerces
+
+
+# GPDB dependencies
+sudo apt-get install -y bison flex
+sudo apt-get install -y libreadline-dev
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y libyaml-dev
+sudo apt-get install -y libapr1-dev
+sudo apt-get install -y libevent-dev
+sudo apt-get install -y libcurl-dev
+sudo apt-get install -y libpam-dev
+sudo apt-get install -y libcurl4-dev
+sudo apt-get install -y libbz2-dev
+sudo apt-get install -y python-dev
+sudo apt-get install -y ssh
+
+sudo apt-get install -y libcurl4-openssl-dev
+
+sudo apt-get install -y python-pip
+
+
+# Other GPDB dependencies --with-perl --with-python --with-libxml
+sudo apt-get install -y libxml2-dev libperl-dev
+
+
+# Python dependencies
+sudo pip install lockfile 
+sudo pip install paramiko
+sudo pip install setuptools
+sudo pip install epydoc
+sudo pip install --pre psi
